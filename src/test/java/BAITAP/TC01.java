5008458e441eae767fb6fac398123aa0bf0982bb
minhnguyet2308 <nguyetnmse172621@fpt.edu.vn>
2023-10-26 00:27:49 +0700

    TC01 and TC02

--- src/test/java/BAITAP/TC01.java
+++ src/test/java/BAITAP/TC01.java
@@ -17,6 +17,7 @@ public class TC01 {
         //init web driver session
         WebDriver driver = driverFactory.getChromeDriver();
         try {
+
             //Go to http://live.techpanda.org/
             driver.get("http://live.techpanda.org/");
 
@@ -36,11 +37,23 @@ public class TC01 {
             //In the list of all mobile , select SORT BY -> dropdown as name
             new Select(driver.findElement(By.cssSelector("select[title=\"Sort By\"]"))).selectByVisibleText("Name");
 
+            //Verify all products are sorted by name
+            String Phone1 = driver.findElement(By.cssSelector("h2[class='product-name'] a[title='IPhone']")).getText();
+            String Phone2 = driver.findElement(By.cssSelector("h2[class='product-name'] a[title='Samsung Galaxy']")).getText();
+            String Phone3 = driver.findElement(By.cssSelector("a[title='Sony Xperia']")).getText();
+            System.out.println("Phone 1: " + Phone1);
+            System.out.println("Phone 2: " + Phone2);
+            System.out.println("Phone 3: " + Phone3);
+            if (Phone1.compareTo(Phone2) <= 0 && Phone2.compareTo(Phone3) <= 0) {
+                System.out.println("All products are sorted by name.");
+            } else {
+                System.out.println("All products are not sorted by name.");
+            }
+
             //debug purpose only
             Thread.sleep(2000);
 
-            //Verify all products are sorted by name
-
+            //Screenshot
             File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
             String png = ("D:\\FPT\\Chuyên ngành 5\\SWT301\\SeleniumWebdriver\\SeleniumWebdriver\\" + "TC01" + ".png");
             FileUtils.copyFile(scrFile, new File(png));
--- src/test/java/BAITAP/TC02.java
+++ src/test/java/BAITAP/TC02.java
@@ -6,22 +6,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 import java.io.File;
 import static org.testng.AssertJUnit.assertEquals;
 @Test
 public class TC02 {
     public static void testTC02() {
-        int scc = 0;
 
-        StringBuffer verificationError = new StringBuffer();
         //Init web-driver session
         WebDriver driver = driverFactory.getChromeDriver();
         try {
             //Go to http://live.techpanda.org/
             driver.get("http://live.techpanda.org/");
+
+            //debug purpose only
+            Thread.sleep(2000);
 
             //Click on  MOBILE  menu
             driver.findElement(By.linkText("MOBILE")).click();
@@ -30,8 +29,8 @@ public class TC02 {
             Thread.sleep(2000);
 
             //In the list of all mobile, read the cost of Sony Xperia mobile (which is $100)
-            String XPeriaPrice = driver.findElement(By.cssSelector("#product-price-1 > span.price")).getText();
-            System.out.println(XPeriaPrice);
+            String PriceInList = driver.findElement(By.cssSelector("#product-price-1 > span.price")).getText();
+            System.out.println("Product value in list: " + PriceInList);
 
             //debug purpose only
             Thread.sleep(2000);
@@ -48,12 +47,16 @@ public class TC02 {
             Thread.sleep(2000);
 
             //Read the Sony Xperia mobile from detail page.
-            String detailPrice = driver.findElement(By.cssSelector("span.price")).getText();
+            String PriceInDetailPage = driver.findElement(By.cssSelector("span.price")).getText();
+            System.out.println("Product value in detail page: " + PriceInDetailPage);
 
             //Compare Product value in list and details page should be equal ($100).
-            assertEquals(XPeriaPrice, detailPrice);
-            if (XPeriaPrice.equals(detailPrice))
-                System.out.printf("XPeriaPrice = detailPrice and price is: %s", detailPrice);
+            assertEquals(PriceInList, PriceInDetailPage);
+            if (PriceInList.equals(PriceInDetailPage)) {
+                System.out.println("Product value in list and detail page is equal: " + PriceInDetailPage);
+            } else {
+                System.out.println("Product value in list and detail page is not equal");
+            }
 
             //debug purpose only
             Thread.sleep(2000);
