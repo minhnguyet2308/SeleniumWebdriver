db29be365cbac566aa19ac5185ef9483f36b3208
minhnguyet2308 <nguyetnmse172621@fpt.edu.vn>
2023-10-30 22:54:00 +0700

    TC05

--- src/test/java/BAITAP/TC05.java
+++ src/test/java/BAITAP/TC05.java
@@ -1,4 +1,193 @@
 package BAITAP;
 
+import driver.driverFactory;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import POM.RegisterPage;
+import POM.SharedPage;
+
+import java.io.File;
+
+import static org.openqa.selenium.OutputType.FILE;
+
+@Test
 public class TC05 {
+    public static void testTC05() {
+
+        // Init web driver session
+        String firstName="Nguyen";
+        String middleName="Minh";
+        String lastName="Nguyet";
+        String emailAddress = "nguyet123@gmail.com";
+        String password= "123456";
+        String confirmPassword= password;
+        String emailShared= "nguyet321@gmail.com";
+        String message = "Check out my wishlist!";
+
+        //init web driver session
+        WebDriver driver = driverFactory.getChromeDriver();
+        try {
+
+            //Go to http://live.techpanda.org/
+            driver.get("http://live.techpanda.org/");
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            //Click on my account link
+            RegisterPage registerPage = new RegisterPage(driver);
+            registerPage.clickMyAccountLink();
+
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            //Click Create an Account link and fill New User information excluding the registered Email ID
+            registerPage.clickCreateAccountLink();
+
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+
+            registerPage.enterFirstName(firstName);
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            registerPage.enterMiddleName(middleName);
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            registerPage.enterLastName(lastName);
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            registerPage.enterEmail(emailAddress);
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            registerPage.enterPassword(password);
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            registerPage.enterConfirmPassword(confirmPassword);
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            //Click Register
+            registerPage.clickRegister();
+
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            //Verify Registration is done. Expected account registration done
+            String successMessage = driver.findElement(By.cssSelector("li.success-msg span")).getText();
+            String successWelcome = driver.findElement(By.cssSelector("p[class='welcome-msg']")).getText();
+            if (successMessage.contains("Thank you for registering with Main Website Store.") && successWelcome.contains("WELCOME, " + firstName.toUpperCase() +" "+ middleName.toUpperCase() +" " + lastName.toUpperCase() + "!")) {
+                System.out.println("Account registration done.");
+            } else {
+                System.out.println("Account registration failed.");
+            }
+
+            //Screenshot
+            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(FILE);
+            String png = ("D:\\FPT\\Chuyên ngành 5\\SWT301\\SeleniumWebdriver\\SeleniumWebdriver\\" + "TC05" + "_1.png");
+            FileUtils.copyFile(scrFile, new File(png));
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            //Go to TV menu
+            driver.findElement(By.linkText("TV")).click();
+
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            //Add product in your wish list - use product - LG LCD
+            driver.findElement(By.xpath("(//a[@class='link-wishlist'][normalize-space()='Add to Wishlist'])[1]")).click();
+
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            //Click SHARE WISHLIST
+            driver.findElement(By.xpath("(//span[contains(text(),'Share Wishlist')])[1]")).click();
+
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            //In next page enter Email and a message and click SHARE WISHLIST
+            SharedPage sharedPage = new SharedPage(driver);
+
+            sharedPage.enterEmailShared(emailShared);
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            sharedPage.enterMessageShared(message);
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            driver.findElement(By.cssSelector("button[title='Share Wishlist']")).click();
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            //Check wishlist is shared. Expected wishlist shared successfully
+            String successMessage2 = driver.findElement(By.xpath("(//span[normalize-space()='Your Wishlist has been shared.'])[1]")).getText();
+            if (successMessage2.contains("Your Wishlist has been shared.")) {
+                System.out.println("Wishlist shared successfully.");
+            } else {
+                System.out.println("Wishlist sharing failed.");
+            }
+
+            //Screenshot
+            scrFile = ((TakesScreenshot)driver).getScreenshotAs(FILE);
+            png = ("D:\\FPT\\Chuyên ngành 5\\SWT301\\SeleniumWebdriver\\SeleniumWebdriver\\" + "TC05" + "_2.png");
+            FileUtils.copyFile(scrFile, new File(png));
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //Quit browser session
+        driver.quit();
+
+    }
 }
---
+++ src/test/java/POM/RegisterPage.java
@@ -0,0 +1,72 @@
+package POM;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class RegisterPage {
+    WebDriver driver;
+
+    By myAccountLink = By.linkText("MY ACCOUNT");
+    By createAccountLink = By.linkText("CREATE AN ACCOUNT");
+    By firstNameInputLocator = By.id("firstname");
+    By middleNameInputLocator = By.id("middlename");
+    By lastNameInputLocator = By.id("lastname");
+    By emailInputLocator = By.id("email_address");
+    By passwordInputLocator = By.id("password");
+    By confirmPasswordInputLocator = By.id("confirmation");
+    By registerButton = By.cssSelector("button[title='Register']");
+
+    public RegisterPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickMyAccountLink() {
+        driver.findElement(myAccountLink).click();
+    }
+
+    public void clickCreateAccountLink() {
+        driver.findElement(createAccountLink).click();
+    }
+
+    public void enterFirstName(String firstName) {
+        WebElement firstNameElement = driver.findElement(firstNameInputLocator);
+        firstNameElement.clear();
+        firstNameElement.sendKeys(firstName);
+    }
+
+    public void enterMiddleName(String middleName){
+        WebElement middleNameElement = driver.findElement(middleNameInputLocator);
+        middleNameElement.clear();
+        middleNameElement.sendKeys(middleName);
+    }
+
+    public void enterLastName(String lastName) {
+        WebElement lastNameElement = driver.findElement(lastNameInputLocator);
+        lastNameElement.clear();
+        lastNameElement.sendKeys(lastName);
+    }
+
+    public void enterEmail(String email) {
+        WebElement emailElement = driver.findElement(emailInputLocator);
+        emailElement.clear();
+        emailElement.sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        WebElement passwordElement = driver.findElement(passwordInputLocator);
+        passwordElement.clear();
+        passwordElement.sendKeys(password);
+    }
+
+    public void enterConfirmPassword(String confirmPassword) {
+        WebElement confirmPasswordElement = driver.findElement(confirmPasswordInputLocator);
+        confirmPasswordElement.clear();
+        confirmPasswordElement.sendKeys(confirmPassword);
+    }
+
+    public void clickRegister() {
+        driver.findElement(registerButton).click();
+    }
+}
