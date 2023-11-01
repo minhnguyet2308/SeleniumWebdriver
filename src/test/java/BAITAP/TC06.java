cc8cbed28a106b8de650d0d7f6029bde58f3db72
minhnguyet2308 <nguyetnmse172621@fpt.edu.vn>
2023-11-01 12:45:34 +0700

    TC06

--- src/test/java/BAITAP/TC06.java
+++ src/test/java/BAITAP/TC06.java
@@ -1,4 +1,45 @@
 package BAITAP;
 
+import POM.RegisterPage;
+import driver.driverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+@Test
 public class TC06 {
+
+    public static void testTC05() {
+        //init web driver session
+        WebDriver driver = driverFactory.getChromeDriver();
+        try {
+
+            //Go to http://live.techpanda.org/
+            driver.get("http://live.techpanda.org/");
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            //Click on my account link
+            RegisterPage registerPage = new RegisterPage(driver);
+            registerPage.clickMyAccountLink();
+
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+            //Login in application using previously created credential
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //Quit browser session
+        driver.quit();
+
+    }
 }
---
+++ src/test/java/POM/LoginPage.java
@@ -0,0 +1,14 @@
+package POM;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+}
