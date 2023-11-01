5f2b32554e7d619a3d42194fa3bbd954bf1afd1e
minhnguyet2308 <nguyetnmse172621@fpt.edu.vn>
2023-11-01 13:14:56 +0700

    TC06 LoginPage

--- src/test/java/BAITAP/TC06.java
+++ src/test/java/BAITAP/TC06.java
@@ -1,6 +1,6 @@
 package BAITAP;
 
-import POM.RegisterPage;
+import POM.LoginPage;
 import driver.driverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
@@ -8,7 +8,11 @@ import org.testng.annotations.Test;
 @Test
 public class TC06 {
 
-    public static void testTC05() {
+    public static void testTC06() {
+
+        String emailAddress = "nhom6@gmail.com";
+        String password= "123456";
+
         //init web driver session
         WebDriver driver = driverFactory.getChromeDriver();
         try {
@@ -20,8 +24,8 @@ public class TC06 {
             Thread.sleep(2000);
 
             //Click on my account link
-            RegisterPage registerPage = new RegisterPage(driver);
-            registerPage.clickMyAccountLink();
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.clickMyAccountLink();
 
             // switching to new window
             for (String handle : driver.getWindowHandles()) {
@@ -32,6 +36,26 @@ public class TC06 {
             Thread.sleep(2000);
 
             //Login in application using previously created credential
+            loginPage.enterEmail(emailAddress);
+
+            //debug purpose only
+            Thread.sleep(1000);
+
+            loginPage.enterPassword(password);
+
+            //debug purpose only
+            Thread.sleep(1000);
+
+            //Click Login
+            loginPage.clickLogin();
+
+            // switching to new window
+            for (String handle : driver.getWindowHandles()) {
+                driver.switchTo().window(handle);
+            }
+
+            //debug purpose only
+            Thread.sleep(2000);
 
 
         } catch (Exception e){
