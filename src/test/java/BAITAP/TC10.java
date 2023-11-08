12ef9dc19581ab7fb102f6afef1501ba9056191c
minhnguyet2308 <nguyetnmse172621@fpt.edu.vn>
2023-11-08 11:44:35 +0700

    Update

--- src/test/java/BAITAP/TC10.java
+++ src/test/java/BAITAP/TC10.java
@@ -1,4 +1,30 @@
 package BAITAP;
 
+import driver.driverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+@Test
 public class TC10 {
+
+    public static void testTC10() {
+
+        //init web driver session
+        WebDriver driver = driverFactory.getChromeDriver();
+        try {
+
+            //Step1. Go to http://live.techpanda.org/
+            driver.get("http://live.techpanda.org/");
+
+            //debug purpose only
+            Thread.sleep(2000);
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //Quit browser session
+        driver.quit();
+
+    }
 }
